e68a335019d4f4bb2df82464fe02b69ba5e3eb3e
Lucas Henneman <lhenneman@gmail.com>
2019-06-02 17:04:33 -0400

    initial

---
+++ R/image_annotater.R
@@ -0,0 +1,40 @@
+image_annotater <- function( input.dt = top.dt,
+                             opinion.use = 'Agree',
+                             ymin.ratio = .6){
+  # use specific opinion
+  dt.use <- input.dt[opinion == opinion.use]
+
+  # if not plotting this image, return blank geom
+  if( is.na( dt.use$emost))
+    return( geom_blank())
+
+  # load image, rasterify
+  inagein <- image_read( as( dt.use$emost, 'character'))
+  raster <- as.raster(inagein)
+
+  # specify ymin location
+  ymin.Agree <- input.dt[opinion == 'Agree']$values * ymin.ratio
+  ymin.Undecided <- ( input.dt[opinion == 'Undecided']$values * ymin.ratio +
+                        input.dt[opinion == 'Agree']$values)
+  ymin.Disagree <- ( input.dt[opinion == 'Disagree']$values * ymin.ratio +
+                       input.dt[opinion == 'Agree']$values +
+                       input.dt[opinion == 'Undecided']$values)
+  if( opinion.use == 'Agree'){
+    ymin = ymin.Agree
+  } else if( opinion.use == 'Undecided'){
+    ymin = ymin.Undecided
+  } else if( opinion.use == 'Disagree'){
+    ymin = ymin.Disagree
+  }
+
+  # specify other locations
+  xmin = 0.85
+  xmax = 1.15
+  ymax = ymin + 0.03
+
+  return( annotation_raster(raster, xmin = xmin,
+                            xmax = xmax,
+                            ymin = ymin,
+                            ymax = ymax))
+
+}
