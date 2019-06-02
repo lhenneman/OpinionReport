4281366cf0d5660403b3a57f9ffd698c9310d4a7
Lucas Henneman <lhenneman@gmail.com>
2019-06-02 16:58:08 -0400

    plot emoji's matching abridgenews.com

--- R/OpnionReport.R
+++ R/OpnionReport.R
@@ -15,11 +15,9 @@ OpRep <- function( title = NULL,
   }
 
   #load emoji font
-  list.emojifonts()
-  load.emojifont('OpenSansEmoji.ttf')
-  emos <-  c( 'Agree' = emo::ji("+1"),
-              'Disagree' = emo::ji("-1"),
-              'Undecided' = emo::ji("confused"))
+  emos <-  c( 'Agree' = '~/Dropbox/Rpackages/OpinionReport/inst/icons/Agree.jpg', # system.file("icons", "Agree.jpg", package = "OpinionReport"),
+              'Undecided' = system.file("icons", "Undecided.jpg", package = "OpinionReport"),
+              'Disagree' = system.file("icons", "Disagree.jpg", package = "OpinionReport"))
 
   # import files - defaults to most recent
   file.name <- tail( list.files( path = directory.in,
@@ -39,9 +37,10 @@ OpRep <- function( title = NULL,
 
   # assign emojis
   dataday1$emost <- emos[dataday1$opinion]
-  dataday1[, emotext := paste0( values * 100,
-                                '% ',
-                                emost)]
+  dataday1$emost <- factor( dataday1$emost,
+                            levels = unique( c( grep( 'Agree', dataday1$emost, value = T),
+                                                grep( 'Undecided', dataday1$emost, value = T),
+                                                grep( 'Disagree', dataday1$emost, value = T))))
 
   # order based on opinions
   order_opinions <- c( 'Agree', 'Undecided', 'Disagree')
@@ -80,9 +79,10 @@ OpRep <- function( title = NULL,
         geom_area( colour=NA,
                    alpha = 0.7) +
         geom_col( data = dataday1,
-                  position = "stack",
-                  stat = "identity",
-                  aes( width = 0.9)) +
+                  position = "stack"#,
+                  #  stat = "identity",
+                  #  aes( width = 0.9)
+        ) +
         scale_x_continuous( breaks = 1:max( df2$xvarn),
                             labels = unique( df2$article),
                             position = "top",
@@ -90,15 +90,23 @@ OpRep <- function( title = NULL,
         scale_fill_manual( values = c( 'Disagree' = '#F44336',
                                        'Agree' = '#4ECB49',
                                        'Undecided' = '#FED34C')) +
-        scale_y_discrete( expand = c( 0, 0)) +
-        geom_text( data = dataday1[values != 0],
-                   aes( label = emotext),
-                   family='OpenSansEmoji',
-                   position = position_stack( vjust = 0.5),
-                   size = 8,
-                   parse = F,
-                   colour = "white",
-                   fontface = "bold") +
+        #   scale_y_discrete( expand = c( 0, 0)) +
+        geom_image( data = dataday1[values != 0],
+                    aes( image = emost),
+                    #  family='OpenSansEmoji',
+                    position = position_stack( vjust = 0.5),
+                    hjust = 0#,
+                    # size = 8,
+                    #  parse = F,
+                    #  colour = NA
+        ) +
+        geom_text(data = dataday1[values != 0],
+                  aes( label = emotext),
+                  position = position_stack( vjust = 0.5),
+                  color = 'white',
+                  size = 8,
+                  hjust = 1
+        ) +
         annotate( "text",
                   colour = "white",
                   x = 4,
@@ -137,28 +145,29 @@ OpRep <- function( title = NULL,
     for( a in unique( dataday1[,article])){
 
       top.dt <- dataday1[ article == a]
-      top.dt[, opinion := factor( top.dt$opinion,
-                                  levels = rev( levels( top.dt$opinion)))]
-      top.dt[ values < max.label, emotext := ""]
+      top.dt[, opinion := factor( opinion,
+                                  levels = rev( levels( opinion)))]
+      top.dt[ values > max.label, emotext := scales::percent( values, accuracy = 1)]
+      top.dt[ values < max.label, emost := NA]
 
       gg4 <- ggplot( data = top.dt,
-                     aes( fill = opinion,
-                          x = 1,
-                          y = values)) +
+                     aes( x = 1,
+                          y = values,
+                          fill = opinion)) +
         geom_bar( stat = 'identity') +
-        coord_flip() +
-        scale_fill_manual( values = c( 'Disagree' = '#F44336',
-                                       'Agree' = '#4ECB49',
-                                       'Undecided' = '#FED34C')) +
-        scale_y_discrete( expand = c( 0, 0)) +
-        geom_text( data = top.dt,
-                   aes( label = emotext),
-                   family='OpenSansEmoji',
+        geom_text( aes( label = emotext),
                    position = position_stack( vjust = 0.5),
                    size = 1.5,
                    parse = F,
-                   #    colour = "white",
                    fontface = "bold") +
+        scale_fill_manual( values = c( 'Disagree' = '#EB0427',
+                                       'Agree' = '#389D28',
+                                       'Undecided' = '#EFC003')) +
+        scale_y_discrete( expand = c( 0, 0)) +
+        coord_flip() +
+        image_annotater( opinion.use = 'Agree') +
+        image_annotater( opinion.use = 'Undecided') +
+        image_annotater( opinion.use = 'Disagree') +
         theme_bw() +
         theme( axis.title = element_blank(),
                axis.text.x = element_blank(),
@@ -166,21 +175,23 @@ OpRep <- function( title = NULL,
                axis.ticks = element_blank(),
                panel.border = element_blank(),
                panel.grid = element_blank(),
-               plot.title = element_text( size = 26,
-                                          hjust = 0.5,
-                                          face = 'bold'),
-               plot.margin=grid::unit(c(0,0,0,0),"cm"),
-               legend.position = 'none')
+               plot.background = element_rect(fill = "transparent",colour = NA),
+               plot.margin = grid::unit(c(0,0,0,0),"cm"),
+               legend.position = 'none',
+               rect = element_rect(fill = "transparent")
+        )
 
+      ## save file, then crop
+      filename.png <- file.path( outname,
+                                 paste0( topic.name, "_",
+                                         a, '.png'))
       ggsave(gg4,
-             filename = file.path( outname,
-                                   paste0( topic.name, "_",
-                                           a, '.jpg')),
+             filename = filename.png,
              width = 2,
              height = .2,
              units = "in",
-             bg = "transparent",
-             dpi = 900
+             dpi = 900,
+             bg = 'transparent'
       )
 
     }
